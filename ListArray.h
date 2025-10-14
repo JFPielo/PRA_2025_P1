828f55991f64848ea6defd2cfb23484c1cbb7b91
Josué Félix Piedrasanta Loarca <jfpielo1@etsinf.upv.es>
2025-10-14 22:19:06 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <ostream>
 #include "List.h"
 
 template <typename T>
@@ -6,14 +6,14 @@ class ListArray : public List<T> {
 	private:
 		T* arr;
 		int max;
-		int n;
+			int n;
 		static const int MINSIZE = 2;
+		void resize(int new_size);
 	public:
 		ListArray();
 		~ListArray();
 		T operator[](int pos);
-		friend std::ostream& operator << (std::ostream &out, const ListArray<T> &list);
-		void resize(int new_size);
+		friend std::ostream& operator <<(std::ostream &out, const ListArray<T> &list);
 		void insert(int pos, T e) override;
 		void append(T e) override;
 		void prepend(T e) override;
@@ -22,4 +22,4 @@ class ListArray : public List<T> {
 		int search(T e) override;
 		bool empty() override;
 		int size() override;
-};	
+	};	
